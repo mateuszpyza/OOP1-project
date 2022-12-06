188416a84e0b246665521458e08bceaf9eb89e3a
mateuszpyza <mateuszpyza1@wp.pl>
2022-12-06 11:54:33 +0100

    fixed second task✓

--- src/test/java/models/Movie.java
+++ src/test/java/models/Movie.java
@@ -2,8 +2,8 @@ package models;
 
 public class Movie {
     private final String title;
-    private  final String directorName;
-    private  final  double durationInMinutes;
+    private final String directorName;
+    private final double durationInMinutes;
     private final int yearOfProduction;
 
     public Movie(String title, String directorName, double durationInMinutes, int yearOfProduction) {
